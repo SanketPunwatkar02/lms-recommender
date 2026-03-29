c4674fb294be939d15a71529ac79612c924ca3e6
Sanket Punwatkar <156663277+SanketPunwatkar02@users.noreply.github.com>
2026-03-29 14:45:38 +0000

    prepare render deployment: add gunicorn, Procfile, .render.yaml, fix app main entrypoint

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app.main:app
+web: gunicorn app.main:app --workers 3 --bind 0.0.0.0:$PORT
--- app/main.py
+++ app/main.py
@@ -33,5 +33,7 @@ def submit_data():
         return jsonify({"error": str(e)}), 400
 
 import os
-port = int(os.environ.get("PORT",5000))
-app.run(host="0.0.0.0", port=5000)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
